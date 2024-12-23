56685badef7c370f9deeecc8befc3f116e00b421
MuslimDIO <weebashiixd@gmail.com>
2024-12-23 20:12:14 +0200

    consume

--- UseConsumable.cpp
+++ UseConsumable.cpp
@@ -11,6 +11,15 @@ UseConsumable::UseConsumable(ApplicationManager* pApp) : Action(pApp)
 
 void UseConsumable::ReadActionParameters()
 {
+	// Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+	pOut->PrintMessage("Would you like to use the 1. ToolKit or 2. Hacking Device");
+	// Read the cell position
+	int choice = pIn->GetInteger(pOut);
+	// Clear messages
+	pOut->ClearStatusBar();
 }
 
 
