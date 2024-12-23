47141cbc5b2fe036e1f524dcba72db11d4b37802
abdelrahmanbana <abdelrahman.albana05@eng-st.cu.edu.eg>
2024-12-23 19:24:33 +0200

    addflagaction to be contineud

--- AddFlagAction.cpp
+++ AddFlagAction.cpp
@@ -17,12 +17,26 @@ void AddFlagAction::ReadActionParameters()
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1- Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
 
 	// 2- Read the flagPos
+	pOut->PrintMessage("New Flag: Click on its Cell ...");
+	flagPos = pIn->GetCellClicked();
 
 	// 4- Make the needed validations on the read parameters
+	if (flagPos.IsValidCell() == false) {
+		pOut->PrintMessage("Error: Invalid cell position! Click to continue...");
+		int x, y;
+		pIn->GetPointClicked(x, y);
+		return;
+	}
+
 
 	// 5- Clear status bar
+	pOut->ClearStatusBar();
+
 }
 
 void AddFlagAction::Execute()
@@ -34,10 +48,26 @@ void AddFlagAction::Execute()
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1-Create a flag object
+	Flag* pFlag = new Flag(flagPos);
+
 	// 2-get a pointer to the Grid from the ApplicationManager
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
 	// 3-Add the flag object to the GameObject of its Cell:
-	// 4-Check if the flag was added and print an errror message if flag couldn't be added
+	bool added = pGrid->AddObjectToCell(pFlag);
 	
+	
+	// 4-Check if the flag was added and print an errror message if flag couldn't be added
+	if (added == false)
+	{
+		pOut->PrintMessage("Error: Cell already has an object! Click to continue...");
+		int x, y;
+		pIn->GetPointClicked(x, y);
+		delete pFlag;
+
+	}
 }
 
 AddFlagAction::~AddFlagAction()
