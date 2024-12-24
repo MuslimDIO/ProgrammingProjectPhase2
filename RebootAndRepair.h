02d902d9d55c7d4779fa86c72860bc381f8ae1e3
MuslimDIO <weebashiixd@gmail.com>
2024-12-24 21:41:19 +0200

    fixed rebootandrepair class

--- RebootAndRepair.h
+++ RebootAndRepair.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "Action.h"
-class RebootAndRepair :
-    public Action
+class RebootAndRepair : public Action
 {
 
-
+public:
 	RebootAndRepair(ApplicationManager* pApp);
 	virtual void ReadActionParameters(); // Reads action parameters 
 	virtual void Execute(); 
