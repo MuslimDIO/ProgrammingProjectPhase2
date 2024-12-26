135a15fdae1055bdaa711305ce8ee7ec02bee1b2
IbrahimMohsenMoussa <ibrahim.moussa04@eng-st.cu.edu.eg>
2024-12-26 14:53:09 +0200

    fix docs

--- ExecuteCommandAction.cpp
+++ ExecuteCommandAction.cpp
@@ -1,3 +1,14 @@
+
+/**
+ * @file ExecuteCommandAction.cpp
+ * @brief Implementation of the ExecuteCommandAction class.
+ *
+ * This file contains the implementation of the ExecuteCommandAction class,
+ * which is responsible for executing saved commands for the current player
+ * in the game.
+ * @author Omar Walid (Team Leader)
+ */
+
 #include "ExecuteCommandAction.h"
 #include "Input.h"
 #include "Output.h"
@@ -5,13 +16,31 @@
 #include "Grid.h"
 #include "Player.h"
 enum Command;
+/**
+ * @brief Constructor for ExecuteCommandAction.
+ *
+ * Initializes a new instance of the ExecuteCommandAction class.
+ *
+ * @param pApp Pointer to the ApplicationManager.
+ **/
 ExecuteCommandAction::ExecuteCommandAction(ApplicationManager *pApp) : Action(pApp)
 {
 }
 
+/**
+ * @brief Destructor for ExecuteCommandAction.
+ *
+ * Cleans up any resources used by the ExecuteCommandAction instance.
+ */
 ExecuteCommandAction::~ExecuteCommandAction()
 {
 }
+/**
+ * @brief Reads the parameters required for the action.
+ *
+ * This function reads the necessary parameters for executing the action
+ * from the input and output interfaces.
+ */
 
 void ExecuteCommandAction::ReadActionParameters()
 {
@@ -21,23 +50,35 @@ void ExecuteCommandAction::ReadActionParameters()
     Output *pOut = pGrid->GetOutput();
 }
 
-
-
+/**
+ * @brief Executes the saved command for the current player.
+ *
+ * This function performs the following steps:
+ * - Reads the action parameters.
+ * - Retrieves the current player and their saved commands.
+ * - Executes the saved commands.
+ * - Clears the saved commands.
+ * - Generates available commands for the current player.
+ * - Updates the game interface.
+ * - Advances to the next player.
+ * - Generates available commands for the new current player.
+ * - Updates the commands bar in the game interface.
+ */
 void ExecuteCommandAction::Execute()
 {
 
     ReadActionParameters();
-    Grid* pGrid = pManager->GetGrid();
-    Input* pIn = pGrid->GetInput();
-    Output* pOut = pGrid->GetOutput();
-    Player* Ptr2_currPlayer = pGrid->GetCurrentPlayer();
+    Grid *pGrid = pManager->GetGrid();
+    Input *pIn = pGrid->GetInput();
+    Output *pOut = pGrid->GetOutput();
+    Player *Ptr2_currPlayer = pGrid->GetCurrentPlayer();
     pOut->PrintMessage("Executing Saved Command");
     int x, y;
     pIn->GetPointClicked(x, y);
 
     int l_svSize;
     int l_avSize;
-    Command* l_SavedComm = Ptr2_currPlayer->GetSavedCommands(l_svSize);
+    Command *l_SavedComm = Ptr2_currPlayer->GetSavedCommands(l_svSize);
 
     Ptr2_currPlayer->Move(pGrid, l_SavedComm);
 
@@ -51,7 +92,7 @@ void ExecuteCommandAction::Execute()
     pGrid->AdvanceCurrentPlayer();
 
     Ptr2_currPlayer = pGrid->GetCurrentPlayer();
-    Command* l_avComm = Ptr2_currPlayer->GenerateAvailableCommands(l_avSize);
+    Command *l_avComm = Ptr2_currPlayer->GenerateAvailableCommands(l_avSize);
     pOut->CreateCommandsBar(l_SavedComm, l_svSize, l_avComm, l_avSize);
     // int selected = -1;
     // while (selected == -1)
--- ExecuteCommandAction.h
+++ ExecuteCommandAction.h
@@ -1,13 +1,52 @@
 
+/**
+ * @file ExecuteCommandAction.h
+ * @brief Header file for the ExecuteCommandAction class.
+ *
+ * This file contains the definition of the ExecuteCommandAction class,
+ * which is responsible for executing saved commands for the current player
+ * in the game. The class inherits from the Action class and interacts with
+ * various other classes such as Grid, Input, Output, and Player to perform
+ * its functionality.
+ * @author Omar Walid (Team Leader)
+ */
 #include "Action.h"
-
+/**
+ * @class ExecuteCommandAction
+ * @brief A class to execute saved commands for the current player.
+ *
+ * This class inherits from the Action class and is used to execute
+ * commands that have been saved by the current player. It interacts
+ * with the Grid, Input, Output, and Player classes to perform its
+ * functionality.
+ */
 class ExecuteCommandAction : public Action
 {
 public:
-    ExecuteCommandAction(ApplicationManager* pApp);
+    ExecuteCommandAction(ApplicationManager *pApp);
+
     virtual ~ExecuteCommandAction();
 
+    /**
+     * @brief Reads the parameters required for the action.
+     *
+     * This function reads the necessary parameters for executing the action
+     * from the input and output interfaces.
+     */
     virtual void ReadActionParameters();
+    /**
+     * @brief Executes the saved command for the current player.
+     *
+     * This function performs the following steps:
+     * - Reads the action parameters.
+     * - Retrieves the current player and their saved commands.
+     * - Executes the saved commands.
+     * - Clears the saved commands.
+     * - Generates available commands for the current player.
+     * - Updates the game interface.
+     * - Advances to the next player.
+     * - Generates available commands for the new current player.
+     * - Updates the commands bar in the game interface.
+     */
     virtual void Execute();
 };
-
